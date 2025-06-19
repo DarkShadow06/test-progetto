9d2b23d9d4e009d940978eefcf3fbcb9ff502190
DarkShadow06 <85438374+DarkShadow06@users.noreply.github.com>
2025-06-19 12:12:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("myForm").addEventListener("submit", function(e) {
   e.preventDefault();
   const nome = document.getElementById("nome").value;
 
-  fetch("https://script.google.com/macros/s/AKfycbz1DLhZCt6D813lqZkwLyYS7o8TWT9dbER6WvV74vUOUT00ohiVUZIOeilhkpNhX2bF/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbwfJVaDZEvBpBtluwNvF8dqapvGDUIGAw28jLIfKnb9NXhhhnuG3dEgAHmfzHWrfhVw/exec", {
       method: "POST",
       body: JSON.stringify({ nome: nome }),
       headers: {
